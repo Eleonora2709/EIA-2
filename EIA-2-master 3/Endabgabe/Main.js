3d9524624f93937ed40bda45596624392defbb59
Eleonora2709 <43825783+Eleonora2709@users.noreply.github.com>
2020-02-11 18:01:13 +0100

    Neue Version

--- EIA-2-master 3/Endabgabe/Main.js
+++ EIA-2-master 3/Endabgabe/Main.js
@@ -1,16 +1,26 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var L11;
 (function (L11) {
+    //https://eia-eleonora.herokuapp.com/ -> Adresse meiner App
     window.addEventListener("load", init);
     let snowflakeArray = [];
     let birdArray = [];
-    let throwSnowball;
     let bird; //nicht sicher, ob richtig
     let throwBirdfood;
     let fps = 25;
-    let score = 1000;
+    L11.score = 1000;
     let node;
     let wroteScore = false;
     let startbutton;
+    L11.url = "https://eia-eleonora.herokuapp.com/";
     function init(_event) {
         document.getElementById("game").style.display = "none";
         document.getElementById("endscreen").style.display = "none";
@@ -18,7 +28,10 @@ var L11;
         startbutton.addEventListener("click", handleLoad);
     }
     function handleLoad(_event) {
+        L11.score = 1030;
         console.log("starting");
+        let submit = document.querySelector("button[id=sendData]");
+        submit.addEventListener("click", nameAndScore);
         document.getElementById("startscreen").style.display = "none";
         document.getElementById("game").style.display = "initial";
         let canvas = document.querySelector("canvas");
@@ -28,13 +41,11 @@ var L11;
         canvas.addEventListener("click", handleClick);
         canvas.addEventListener("contextmenu", handleRightClick);
         //canvas.addEventListener(TASK.EAT, breakBird);
-        window.setInterval(generateScore, 1000);
-        function generateScore() {
-            console.log(score);
-            score--;
-        }
+        let highscorebutton = document.getElementById("highscorelistbutton");
+        highscorebutton.addEventListener("click", gethighscorelist);
+        document.getElementById("highscorelist").addEventListener("click", gethighscorelist);
         //generateBird
-        for (let i = 0; i < 20; i++) {
+        for (let i = 0; i < 1; i++) {
             bird = new L11.Bird(2);
             console.log("new bird");
             birdArray.push(bird);
@@ -45,50 +56,42 @@ var L11;
             //  console.log("new flake");
             snowflakeArray.push(snowflake);
         }
-        window.setInterval(update, fps);
-        function update() {
-            // console.log("Update");
-            L11.crc2.clearRect(0, 0, L11.crc2.canvas.width, L11.crc2.canvas.height);
-            L11.crc2.putImageData(L11.image, 0, 0);
-            //window.setInterval(generateScore, 1000);
-            for (let i = 0; i < birdArray.length; i++) {
-                birdArray[i].draw();
-                birdArray[i].move(100);
-            }
-            for (let i = 0; i < snowflakeArray.length; i++) {
-                snowflakeArray[i].draw();
-                snowflakeArray[i].move(100);
-            }
-            if (throwSnowball) {
-                throwSnowball.draw();
-                if (throwSnowball.size >= 0.2)
-                    throwSnowball.size -= 0.2;
-            }
-            if (throwBirdfood) {
-                throwBirdfood.draw();
-                if (throwBirdfood.size >= 2.7)
-                    throwBirdfood.size -= 0.2;
-                if (throwBirdfood.size <= 2.6 && throwBirdfood.size >= 0.002)
-                    throwBirdfood.size -= 0.002;
-                //this.bird.move();
-            }
-            // function generateScore(): void {
-            //     console.log(score);
-            //     score--;
-            // }
+    }
+    let finalscore = window.setInterval(generateScore, 1000);
+    function generateScore() {
+        console.log(L11.score);
+        L11.score--;
+    }
+    window.setInterval(update, fps);
+    function update() {
+        // console.log("Update");
+        L11.crc2.clearRect(0, 0, L11.crc2.canvas.width, L11.crc2.canvas.height);
+        L11.crc2.putImageData(L11.image, 0, 0);
+        //window.setInterval(generateScore, 1000);
+        for (let i = 0; i < birdArray.length; i++) {
+            birdArray[i].draw();
+            birdArray[i].move(100);
+        }
+        for (let i = 0; i < snowflakeArray.length; i++) {
+            snowflakeArray[i].draw();
+            snowflakeArray[i].move(100);
+        }
+        if (L11.throwSnowball) {
+            L11.throwSnowball.draw();
+            if (L11.throwSnowball.size >= 0.2)
+                L11.throwSnowball.size -= 0.2;
+        }
+        if (throwBirdfood) {
+            throwBirdfood.draw();
+            if (throwBirdfood.size >= 2.7)
+                throwBirdfood.size -= 0.2;
+            if (throwBirdfood.size <= 2.6 && throwBirdfood.size >= 0.002)
+                throwBirdfood.size -= 0.002;
+            //this.bird.move();
         }
     }
-    function handleClick(_event) {
-        score--;
-        console.log(_event);
-        let snowballVector = new L11.Vector(_event.offsetX, _event.offsetY);
-        throwSnowball = new L11.Snowball(5, snowballVector);
-        window.setTimeout(getBirdHit, 500, snowballVector);
-        //let hotspot: Vector = new Vector(_event.x - crc2.canvas.offsetLeft, _event.y - crc2.canvas.offsetTop);
-        //score =+ 10;
-    }
     function handleRightClick(_event) {
-        score--;
+        L11.score--;
         console.log(_event);
         let birdfoodVector = new L11.Vector(_event.offsetX, _event.offsetY);
         throwBirdfood = new L11.Birdfood(5, birdfoodVector);
@@ -105,14 +108,19 @@ var L11;
             }
         }
     }
-    function breakBird(_bird) {
-        let index = birdArray.indexOf(_bird);
-        birdArray.splice(index, 1); //index sucht an welcher Stelle Bird im Array ist --> löscht an dieser Stelle eine Instanz heraus
-        if (birdArray.length <= 0) {
-            console.log("ALL BIRDS ARE HIT");
-            //location.replace("EndScreen.html"); //Verlinkung zum Endscreen
-            showGameOverScreen();
-        }
+    function isNear(_hotspot) {
+        let nearsize = 100;
+        let getDifference = L11.Vector.getDifference(_hotspot, new L11.Vector(throwBirdfood.position.x, throwBirdfood.stand));
+        return (nearsize >= getDifference.length);
+    }
+    function handleClick(_event) {
+        L11.score--;
+        console.log(_event);
+        let snowballVector = new L11.Vector(_event.offsetX, _event.offsetY);
+        L11.throwSnowball = new L11.Snowball(5, snowballVector);
+        window.setTimeout(getBirdHit, 500, snowballVector);
+        //let hotspot: Vector = new Vector(_event.x - crc2.canvas.offsetLeft, _event.y - crc2.canvas.offsetTop);
+        //score =+ 10;
     }
     function getBirdHit(_hotspot) {
         for (let bird of birdArray) {
@@ -122,26 +130,58 @@ var L11;
             }
         }
     }
-    function isNear(_hotspot) {
-        let nearsize = 100;
-        let getDifference = L11.Vector.getDifference(_hotspot, new L11.Vector(throwBirdfood.position.x, throwBirdfood.stand));
-        return (nearsize >= getDifference.length);
+    function breakBird(_bird) {
+        let index = birdArray.indexOf(_bird);
+        birdArray.splice(index, 1); //index sucht an welcher Stelle Bird im Array ist --> löscht an dieser Stelle eine Instanz heraus
+        if (birdArray.length <= 0) {
+            console.log("ALL BIRDS ARE HIT");
+            //location.replace("EndScreen.html"); //Verlinkung zum Endscreen
+            showGameOverScreen();
+        }
     }
-    //https://eia-eleonora.herokuapp.com/ -> Adresse meiner App
     function showGameOverScreen() {
+        let submit = document.querySelector("button[id=sendData]");
+        submit.addEventListener("click", nameAndScore);
         document.getElementById("game").style.display = "none";
         document.getElementById("endscreen").style.display = "initial";
         node = document.getElementsByClassName("yourScore")[0];
+        clearInterval(finalscore);
         scoreToHTML();
     }
     L11.showGameOverScreen = showGameOverScreen;
     function scoreToHTML() {
         if (!wroteScore) {
             let content = "";
-            content = "Your score: " + score;
+            content = "Your score: " + L11.score;
             node.innerHTML += content;
             wroteScore = true;
         }
+    }
+    function nameAndScore() {
+        console.log("end");
+        let insertedname = prompt("Your Score: " + L11.score + "\n Enter your name.");
+        if (insertedname != null) {
+            sendtohighscorelist(insertedname, L11.score);
+        }
+    }
+    function sendtohighscorelist(_insertedName, _score) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let query = "name=" + _insertedName + "&highScore=" + _score;
+            let response = yield fetch(L11.url + "?" + query);
+            console.log(response);
+        });
+    }
+    function gethighscorelist() {
+        return __awaiter(this, void 0, void 0, function* () {
+            console.log("Highscores ausgeben");
+            let query = "command=retrieve";
+            let response = yield fetch(L11.url + "?" + query);
+            let responseText = yield response.text();
+            //let finalresponse: any[] = JSON.parse(responseText);
+            alert(responseText);
+            let scores = document.querySelector("span#showScores");
+            scores.innerText = responseText;
+        });
     }
 })(L11 || (L11 = {}));
 //# sourceMappingURL=Main.js.map
--- EIA-2-master 3/Endabgabe/Main.ts
+++ EIA-2-master 3/Endabgabe/Main.ts
@@ -1,23 +1,23 @@
 namespace L11 {
 
-
+//https://eia-eleonora.herokuapp.com/ -> Adresse meiner App
     window.addEventListener("load", init);
     //window.addEventListener('contextmenu', function (e) { e.preventDefault(); });
     export let crc2: CanvasRenderingContext2D;
-//https://eia-eleonora.herokuapp.com/
+
     let snowflakeArray: Snowflake[] = [];
     let birdArray: Bird[] = [];
-    let throwSnowball: Snowball;
+    export let throwSnowball: Snowball;
     let bird: Bird; //nicht sicher, ob richtig
     let throwBirdfood: Birdfood;
     let fps: number = 25;
-    let score: number = 1000;
+    export let score: number = 1000;
     let node: HTMLDivElement;
     let wroteScore: boolean = false;
     let startbutton: HTMLButtonElement;
 
 
-
+    export let url: string = "https://eia-eleonora.herokuapp.com/";
 
     function init(_event: Event): void {
         document.getElementById("game").style.display = "none";
@@ -28,7 +28,13 @@ namespace L11 {
 
     }
     function handleLoad(_event: Event): void {
+
+        score = 1030;
         console.log("starting");
+
+        let submit: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button[id=sendData]");
+        submit.addEventListener("click", nameAndScore);
+
 
         document.getElementById("startscreen").style.display = "none";
         document.getElementById("game").style.display = "initial";
@@ -42,15 +48,16 @@ namespace L11 {
         canvas.addEventListener("click", handleClick);
         canvas.addEventListener("contextmenu", handleRightClick);
         //canvas.addEventListener(TASK.EAT, breakBird);
-        window.setInterval(generateScore, 1000);
 
-        function generateScore(): void {
-            console.log(score);
-            score--
-        }
+
+        let highscorebutton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("highscorelistbutton");
+        highscorebutton.addEventListener("click", gethighscorelist);
+        
+        document.getElementById("highscorelist").addEventListener("click", gethighscorelist);
+
 
         //generateBird
-        for (let i: number = 0; i < 20; i++) {
+        for (let i: number = 0; i < 1; i++) {
             bird = new Bird(2);
             console.log("new bird");
             birdArray.push(bird);
@@ -64,7 +71,15 @@ namespace L11 {
             snowflakeArray.push(snowflake);
 
         }
+    }
 
+
+    let finalscore : number = window.setInterval(generateScore, 1000);
+
+    function generateScore(): void {
+        console.log(score);
+        score--
+    }
         window.setInterval(update, fps);
 
 
@@ -99,15 +114,42 @@ namespace L11 {
                     throwBirdfood.size -= 0.002;
                 //this.bird.move();
             }
-
-            // function generateScore(): void {
-            //     console.log(score);
-            //     score--;
-            // }
         }
 
-    }
+    
+        function handleRightClick(_event: MouseEvent): void {
 
+            score--;
+    
+            console.log(_event);
+            let birdfoodVector: Vector = new Vector(_event.offsetX, _event.offsetY);
+            throwBirdfood = new Birdfood(5, birdfoodVector);
+            //if (isNear(throwBirdFood.position.x))
+    
+            for (let bird of birdArray) {
+                if (isNear(bird.position)) {
+                    this.job = TASK.FLYTOFOOD;
+                    bird.velocity = Vector.getDifference(new Vector(throwBirdfood.position.x + Math.random() * (80 - 10) + 10, throwBirdfood.stand), bird.position);
+                    bird.velocity.scale(0.01); //Strecke wird in Bereiche unterteilt
+                    setTimeout(bird.isPicking, 100 * fps); // wird mit scale multipliziert damit das 1 ergibt
+    
+    
+    
+                    if (bird.velocity.x != 0) {
+                        bird.job = TASK.EAT;
+    
+                    }
+    
+                }
+            }
+        }
+        function isNear(_hotspot: Vector): boolean {
+            let nearsize: number = 100;
+            let getDifference: Vector = Vector.getDifference(_hotspot, new Vector(throwBirdfood.position.x, throwBirdfood.stand));
+            return (nearsize >= getDifference.length);
+        }
+    
+        
     function handleClick(_event: MouseEvent): void {
 
         score--;
@@ -121,38 +163,14 @@ namespace L11 {
         //score =+ 10;
     }
 
-
-
-    function handleRightClick(_event: MouseEvent): void {
-
-        score--;
-
-        console.log(_event);
-        let birdfoodVector: Vector = new Vector(_event.offsetX, _event.offsetY);
-        throwBirdfood = new Birdfood(5, birdfoodVector);
-        //if (isNear(throwBirdFood.position.x))
-
+    function getBirdHit(_hotspot: Vector): void {
         for (let bird of birdArray) {
-            if (isNear(bird.position)) {
-                this.job = TASK.FLYTOFOOD;
-                bird.velocity = Vector.getDifference(new Vector(throwBirdfood.position.x + Math.random() * (80 - 10) + 10, throwBirdfood.stand), bird.position);
-                bird.velocity.scale(0.01); //Strecke wird in Bereiche unterteilt
-                setTimeout(bird.isPicking, 100 * fps); // wird mit scale multipliziert damit das 1 ergibt
-
-
-
-                if (bird.velocity.x != 0) {
-                    bird.job = TASK.EAT;
-
-                }
-
+            if (bird.isHit(_hotspot)) {
+                breakBird(bird);
+                //return;
             }
         }
     }
-
-
-
-
     function breakBird(_bird: Bird): void {
         let index: number = birdArray.indexOf(_bird);
         birdArray.splice(index, 1); //index sucht an welcher Stelle Bird im Array ist --> löscht an dieser Stelle eine Instanz heraus
@@ -163,26 +181,19 @@ namespace L11 {
         }
     }
 
-    function getBirdHit(_hotspot: Vector): void {
-        for (let bird of birdArray) {
-            if (bird.isHit(_hotspot)) {
-                breakBird(bird);
-                //return;
-            }
-        }
-    }
-    function isNear(_hotspot: Vector): boolean {
-        let nearsize: number = 100;
-        let getDifference: Vector = Vector.getDifference(_hotspot, new Vector(throwBirdfood.position.x, throwBirdfood.stand));
-        return (nearsize >= getDifference.length);
-    }
 
 
-    //https://eia-eleonora.herokuapp.com/ -> Adresse meiner App
+
+
     export function showGameOverScreen(): void {
+
+        let submit: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button[id=sendData]");
+
+        submit.addEventListener("click", nameAndScore);
         document.getElementById("game").style.display = "none";
         document.getElementById("endscreen").style.display = "initial";
         node = <HTMLDivElement>document.getElementsByClassName("yourScore")[0];
+        clearInterval(finalscore);
         scoreToHTML();
     }
 
@@ -194,4 +205,38 @@ namespace L11 {
             wroteScore = true;
         }
     }
+
+    function nameAndScore(): void {
+        console.log("end");
+        let insertedname: any = prompt("Your Score: " + score + "\n Enter your name.");
+        if (insertedname != null) {
+            sendtohighscorelist(insertedname, score);
+        }
+    }
+    async function sendtohighscorelist(_insertedName: string, _score: number): Promise<void> {
+
+        let query: string = "name=" + _insertedName + "&highScore=" + _score;
+        let response: Response = await fetch(url + "?" + query);
+        console.log(response);
+
+    }
+    
+    async function gethighscorelist(): Promise<void> {
+
+        console.log("Highscores ausgeben");
+        let query: string = "command=retrieve";
+        let response: Response = await fetch(url + "?" + query);
+        let responseText: string = await response.text();
+        //let finalresponse: any[] = JSON.parse(responseText);
+
+        alert(responseText);
+        let scores: HTMLDivElement = <HTMLDivElement>document.querySelector("span#showScores");
+        scores.innerText = responseText;
+
+
+    }
+
+
+
+
 }
